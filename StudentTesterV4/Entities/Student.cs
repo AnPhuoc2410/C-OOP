e005e49181fb7e014b49391cb87f52473bb8d41f
Dao An Phuoc <anphuocdao2410@gmail.com>
2024-10-03 10:33:45 +0700

    PROPERTY

---
+++ StudentTesterV4/Entities/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTesterV4.Entities
+{
+    public class Student
+    {
+        private String _id;
+        private String _name;
+        private int _yob;
+        private double _gpa;
+
+        public Student()
+        {
+            
+        }
+
+        public Student(string id, string name, int yob, double gpa)
+        {
+            _id = id;
+            _name = name;
+            _yob = yob;
+            _gpa = gpa;
+        }
+        public String Id
+        {
+            get => _id; set => _id = value;
+        }
+        //Lỡ quên mà k gợi ý
+        //PROPFULL  tab twice 
+
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        //Sau khi cos prop , thì ta làm tiếp ToString() và các hàm còn lại
+        //New qua cách thức:
+        //Truyền thống đủ 2 vế, ăn bớt trái - phải, name-argument, Object initialization
+
+        //Get/Set kiểu mới
+        //Theo style biến = (Get) (Set) nhưng vẫn là boiler plate do lặp đi lặp lại
+
+
+    }
+}
---
+++ StudentTesterV5/Entities/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTesterV5.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Yob { get; set; }
+        public double Gpa { get; set; }
+
+        //Kĩ thuật Code kiểu này là kĩ thuật giấu BACKING FILE
+        //Vì đằng nào backing file cũng là ẩn sau các biến PROPERTY này
+        //Nên RUNTIME tự sinh ra _Backing FIle để đỡ phải khai báo từng biến private, những đoạn code nhàm chán
+        //AUTO - GENERATED PROPERTY
+
+        //Nếu quên cú pháp auto - generated property
+        //Gõ prop tab twice
+        //public int MyProperty { get; set; }
+
+    }
+}
+//KHI NÀO SÀI KĨ THUẬT PROPERTY: FULL PROPERTY ( BACKING FILED do mình tạo) VÀ AUTO-IMPLEMENTED PROPERTY ( GIẤU BACKING FILED)
+//Cho AUTO-IMPLEMENTED PROPERTY
+//Thường ta hay sài với class mà nó sẽ map Data xuống table
+//Table là nơi Set và Get Data, xử lí để ở câu query, trong ram ở class khác
+//K ai cấm bạn sài property dạng Full hoặc Auto ở các class khác
